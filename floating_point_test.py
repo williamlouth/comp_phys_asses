1ec86a78a5d91f9dd2118735796ce30e47e547d4
william <williamlouth@outlook.com>
2018-10-20 16:06:52 +0100

    new file:   LU_decomp.py 	modified:   floating_point_test.py

--- floating_point_test.py
+++ floating_point_test.py
@@ -1,4 +1,38 @@
+import numpy as np
+import pylab as pl
 
+
+#np.set_printoptions(formatter = float,floatmode = None)
+a = np.float64(1.0)
+
+
+
+
+
+
+
+print(np.finfo(np.longdouble))
+print(a-2**-52)
+c = 0
+
+for i in range(1,100,1):
+    b = np.float64(2**-i)      #python does irratating type allocations
+    if  a-b != 1.0:
+        print(b)
+        print("%.64f" % (a-b))
+    else:
+        print(b)
+        print("%.64f" % (a-b))
+        print(i-1)
+        c= i-1
+        break
+
+# =============================================================================
+# print(c)
+# print(2**-c)
+# print(a)
+# print(np.float64(a-2**-c))
+# =============================================================================
 
 
 
